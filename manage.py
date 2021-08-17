1de6fbca3cc5b4952d7df60d1e1d8071a9e410bd
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-08-17 15:57:08 +0300

    creates shell manager function shell manager decorator

--- manage.py
+++ manage.py
@@ -7,6 +7,8 @@ app = create_app('development')
 
 manager = Manager(app)
 
-
+@manager.shell
+def make_shell_context():
+    return dict(app = app, db = db, User = User, Pitch = Pitch)
 if __name__ == '__main__':
     manager.run()
