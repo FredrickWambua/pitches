19792e06d3f547b54655518be313786a369b3862
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-08-18 16:28:58 +0300

    creates a registration view function

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,26 @@
-from flask import render_template
+from flask import render_template,redirect,url_for
+from flask_wtf import form
+from ..models import User
+from .forms import RegistrationForm
+from .. import db
 from . import auth
+
+
+# registration view function
+@auth.route('/register', methods = ['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data, password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+        title = 'New account'
+    return render_template('auth/register.html', registration_form = form)
+
 
 # login view function
 @auth.route('/login')
 def login():
     return render_template('auth/login.html')
+
