15502df81d7468120c7baf1c4c6132db7207e17e
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-08-18 13:37:55 +0300

    importing login manager and instantiating its class

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,14 @@ from flask import Flask, app
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, migrate
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 migrate = Migrate()
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -12,9 +17,10 @@ def create_app(config_name):
     # app configurations
     app.config.from_object(config_options[config_name])
 
-    # Initializing database
+    # Initializing flask extensions
     db.init_app(app)
     migrate.init_app(app, db)
+    login_manager.init_app(app)
 
     # registering the blueprint function
     from .auth import auth as auth_blueprint
