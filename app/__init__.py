9f23f4148c3f86844797c2f3aea78258f27d4bdb
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-08-19 15:23:24 +0300

    imports uploads and configures the app

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, migrate
 from flask_login import LoginManager
+from flask_uploads import UploadSet, configure_uploads, IMAGES
 
 bootstap = Bootstrap()
 db = SQLAlchemy()
@@ -12,12 +13,16 @@ migrate = Migrate()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+photos = UploadSet('photos', IMAGES)
 
 def create_app(config_name):
     app = Flask(__name__)
 
     # app configurations
     app.config.from_object(config_options[config_name])
+
+    # configure UploadSet
+    configure_uploads(app,photos)
 
     # Initializing flask extensions
     Bootstrap(app)
