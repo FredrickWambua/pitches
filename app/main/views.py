b795deadfd66e3639083b0c5d2e4e6fa0c3e82da
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-08-19 13:20:50 +0300

    updates the the profile function and instantiate form class

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
 import re
 from flask import render_template, request, redirect, url_for,abort
 from flask_login import login_required
+from flask_wtf import form
 from . import main
 from ..models import User, Pitch, Comment
 from .forms import updateProfile
@@ -14,10 +15,19 @@ def new_pitch(id):
     return render_template('index.html')
 
 @main.route('/user/<uname>')
-def profile(uname):
+def update_profile(uname):
     user = User.query.filter_by(name = uname).first()
 
     if user is None:
         abort(404)
 
-    return render_template('profile/profile.html', user =  user)
+    form = updateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile', uname = user.name))
+
+    return render_template('profile/profile.html', form = form)
