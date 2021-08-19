1a8e4cce6c802cd209aff836baaba9d18b213cd9
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-08-19 16:11:06 +0300

    creates update pic method

--- app/main/views.py
+++ app/main/views.py
@@ -5,7 +5,7 @@ from flask_wtf import form
 from . import main
 from ..models import User, Pitch, Comment
 from .forms import updateProfile
-from .. import db
+from .. import db,photos
 
 @main.route('/pitch/', methods =['GET', 'POST'])
 @login_required
@@ -31,3 +31,13 @@ def update_profile(uname):
         return redirect(url_for('.profile', uname = user.name))
 
     return render_template('profile/profile.html', form = form)
+@main.route('/user/<uname>/update/pic', methods = ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile', uname = uname))
