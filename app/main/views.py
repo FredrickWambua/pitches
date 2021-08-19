aa73dc421200e34ed1cf680465c3cbfc5b7f9796
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-08-19 11:47:57 +0300

    creates a route and a profile view function

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,8 @@
-from flask import render_template, request, redirect, url_for
+import re
+from flask import render_template, request, redirect, url_for,abort
 from flask_login import login_required
 from . import main
+from ..models import User, Pitch, Comment
 
 @main.route('/pitch/', methods =['GET', 'POST'])
 @login_required
@@ -8,3 +10,12 @@ def new_pitch(id):
 
 
     return render_template('index.html')
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(name = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template('profile/profile.html', user =  user)
