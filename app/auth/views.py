50e674bc7c411c8a511125704df4df525fc80a26
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-08-22 15:49:46 +0300

    creates login and logout view functions

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,11 @@
+from flask.globals import request
+from flask.helpers import flash
+from flask_login.utils import login_required, login_user, logout_user
 from app.email import mail_message
 from flask import render_template,redirect,url_for
 from flask_wtf import form
 from ..models import User
-from .forms import RegistrationForm
+from .forms import LoginForm, RegistrationForm
 from .. import db
 from . import auth
 
@@ -24,7 +27,20 @@ def register():
 
 
 # login view function
-@auth.route('/login')
+@auth.route('/login', methods = ['GET', 'POST'])
 def login():
-    return render_template('auth/login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username = form.username.data).first()
+        if user != None and user.verify_password(form.password.data):
+           login_user(user,form.remember.data) 
+           return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid username or password')
+    return render_template('auth/login.html', loginform = form)
 
+# logout view function
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
